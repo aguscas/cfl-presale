b542750506d939f05a56aea8ecea7ab340e96211
aguscas <acastro@tryolabs.com>
2021-07-28 16:42:55 -0300

    Remove unnecessary files

--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/zsh
-
-[[ $1 == "1" ]] && echo "Running 1" && socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\" &
-[[ $1 == "2" ]] && echo "Running 2" && open -a Xquartz
--- setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-IPEN0=$(ifconfig en0 | grep "inet " | awk '{print $2}')
-echo $IPEN0
-export IPEN0
